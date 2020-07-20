f35a655998b6fbcc3418563fba2d63c3c321aede
Petr14 <npetya69@gmail.com>
2020-07-20 12:38:01 +0500

    First commit

---
+++ APIVK.py
@@ -0,0 +1,33 @@
+# нужно сделать class и реализовать в нем метод поиска общих друзей
+import requests
+
+token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+class User:
+    def __init__(self, token):
+        self.token = token
+
+    def get_status(self):
+        response = requests.get(
+            'https://api.vk.com/method/status.get',
+            params = {
+                'access_token': token,
+                'v': 5.21
+            }
+        )
+        return response.json()
+    
+    def friends(self):
+        response = requests.get(
+            'https://api.vk.com/method/friends.getMutual',
+            params = {
+                'access_token': token,
+                'sourse_uid': 171691064,
+                'target_uid': 171691064, 
+                'v': 5.21
+            }
+        )
+        return response.json()    
+    
+vakson = User(token)
+print(vakson.get_status())
+print(vakson.friends())
