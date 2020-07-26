e7930d1cb76178b0b5a0cf0f0bbfff5251596e86
Petr14 <npetya69@gmail.com>
2020-07-26 10:49:18 +0500

    Third commit

--- APIVK.py
+++ APIVK.py
@@ -1,35 +1,57 @@
 # нужно сделать class и реализовать в нем метод поиска общих друзей
 import requests
 
-TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-user_id = input()
+TOKEN = '949c061174e5048eb7cbba8550186fb2f269c5c27843a90c9f0eb70ab7305fdfc5cc5b18db4f707b1d79c'
+user_id_first = input(f'Введите id первого пользователя:')
+user_id_second = input(f'Введите id второго пользователя:')
+
 class User:
     def __init__(self, user_id):
         self.user_id = user_id
 
-    def get_status(self):
-        response = requests.get(
-            'https://api.vk.com/method/friends.get',
-            params = {
-                'access_token': TOKEN,
-                'user_id': user_id,
-                'v': 5.21
-            }
-        )
-        return response.json()
     
     # def friends(self):
     #     response = requests.get(
     #         'https://api.vk.com/method/friends.getMutual',
     #         params = {
-    #             'access_token': token,
-    #             'sourse_uid': user_id,
-    #             'target_uid': 171691064, 
+    #             'access_token': TOKEN,
+    #             'sourse_uid': user_id_first,
+    #             'target_uid': user_id_second,
+    #             'count': 100,
     #             'v': 5.21
     #         }
     #     )
     #     return response.json()    
     
-vakson = User(user_id)
-print(vakson.get_status())
+    def get_compare_first(self):
+        response = requests.get(
+            'https://api.vk.com/method/friends.get',
+            params = {
+                'access_token': TOKEN,
+                'user_id': user_id_first,
+                'v': 5.21
+            }
+        )
+        return response.json()
+
+    def get_compare_second(self):
+        response = requests.get(
+            'https://api.vk.com/method/friends.get',
+            params = {
+                'access_token': TOKEN,
+                'user_id': user_id_second,
+                'v': 5.21
+            }
+        )
+        return response.json()  
+          
+    # def __add__(self, other):
+
+    # def __str__(self):
+    #     return super().__str__()
+
+vakson = User(TOKEN)
+result = set(vakson.get_compare_first()) & set(vakson.get_compare_second())
+print(result)
 # print(vakson.friends())
+# print(vakson.get_compare_second())
