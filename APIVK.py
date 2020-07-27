b47a2115cd1d6367cb0c9cfedd6ee0db39200b2d
Petr14 <npetya69@gmail.com>
2020-07-27 22:06:22 +0500

    Fourth commit

--- APIVK.py
+++ APIVK.py
@@ -1,5 +1,6 @@
 # нужно сделать class и реализовать в нем метод поиска общих друзей
 import requests
+import json
 
 TOKEN = '949c061174e5048eb7cbba8550186fb2f269c5c27843a90c9f0eb70ab7305fdfc5cc5b18db4f707b1d79c'
 user_id_first = input(f'Введите id первого пользователя:')
@@ -9,49 +10,50 @@ class User:
     def __init__(self, user_id):
         self.user_id = user_id
 
+    def __str__(self):
+        return f"https://vk.com/id{self.user_id}"
+        
     
-    # def friends(self):
+    def friends(self):
+        response = requests.get(
+            'https://api.vk.com/method/friends.getMutual',
+            params = {
+                'access_token': TOKEN,
+                'sourse_uid': user_id_first,
+                'target_uid': user_id_second,
+                'count': 100,
+                'v': 5.21
+            }
+        )
+        return response.json()    
+    
+    def get_friends(self):
+        response = requests.get(
+            'https://api.vk.com/method/friends.get',
+            params = {
+                'access_token': TOKEN,
+                'user_id': self.user_id,
+                'v': 5.21
+            }
+        )
+        return response.json()["response"]["items"]
+
+    # def get_compare_second(self):
     #     response = requests.get(
-    #         'https://api.vk.com/method/friends.getMutual',
+    #         'https://api.vk.com/method/friends.get',
     #         params = {
     #             'access_token': TOKEN,
-    #             'sourse_uid': user_id_first,
-    #             'target_uid': user_id_second,
-    #             'count': 100,
+    #             'user_id': user_id_second,
     #             'v': 5.21
     #         }
     #     )
-    #     return response.json()    
-    
-    def get_compare_first(self):
-        response = requests.get(
-            'https://api.vk.com/method/friends.get',
-            params = {
-                'access_token': TOKEN,
-                'user_id': user_id_first,
-                'v': 5.21
-            }
-        )
-        return response.json()
-
-    def get_compare_second(self):
-        response = requests.get(
-            'https://api.vk.com/method/friends.get',
-            params = {
-                'access_token': TOKEN,
-                'user_id': user_id_second,
-                'v': 5.21
-            }
-        )
-        return response.json()  
+    #     return response.json()  
           
-    # def __add__(self, other):
-
-    # def __str__(self):
-    #     return super().__str__()
-
-vakson = User(TOKEN)
-result = set(vakson.get_compare_first()) & set(vakson.get_compare_second())
+   
+user1 = User(user_id_first)
+user2 = User(user_id_second)
+result = set(user1.get_friends()) & set(user2.get_friends())
+print(user1)
+print(user2)
 print(result)
-# print(vakson.friends())
-# print(vakson.get_compare_second())
+
