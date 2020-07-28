427af14eeef07cf738b88d1814e6c32b65c3ab35
Petr14 <npetya69@gmail.com>
2020-07-28 21:57:06 +0500

    Fifth commit

--- APIVK.py
+++ APIVK.py
@@ -2,9 +2,8 @@
 import requests
 import json
 
+
 TOKEN = '949c061174e5048eb7cbba8550186fb2f269c5c27843a90c9f0eb70ab7305fdfc5cc5b18db4f707b1d79c'
-user_id_first = input(f'Введите id первого пользователя:')
-user_id_second = input(f'Введите id второго пользователя:')
 
 class User:
     def __init__(self, user_id):
@@ -12,48 +11,36 @@ class User:
 
     def __str__(self):
         return f"https://vk.com/id{self.user_id}"
-        
     
-    def friends(self):
-        response = requests.get(
-            'https://api.vk.com/method/friends.getMutual',
-            params = {
-                'access_token': TOKEN,
-                'sourse_uid': user_id_first,
-                'target_uid': user_id_second,
-                'count': 100,
-                'v': 5.21
-            }
-        )
-        return response.json()    
-    
-    def get_friends(self):
+    @staticmethod    
+    def get_friends(user_id):
         response = requests.get(
             'https://api.vk.com/method/friends.get',
             params = {
                 'access_token': TOKEN,
-                'user_id': self.user_id,
+                'user_id': user_id,
                 'v': 5.21
             }
         )
         return response.json()["response"]["items"]
+    
+    def __and__(self, other):
+        friends1 = self.get_friends(self.user_id)
+        friends2 = self.get_friends(other.user_id)
 
-    # def get_compare_second(self):
-    #     response = requests.get(
-    #         'https://api.vk.com/method/friends.get',
-    #         params = {
-    #             'access_token': TOKEN,
-    #             'user_id': user_id_second,
-    #             'v': 5.21
-    #         }
-    #     )
-    #     return response.json()  
-          
-   
-user1 = User(user_id_first)
-user2 = User(user_id_second)
-result = set(user1.get_friends()) & set(user2.get_friends())
-print(user1)
-print(user2)
-print(result)
+        result = set(friends1) & set(friends2)
+
+        return [User(user_id) for user_id in result]
+
+
+user1 = User(47526429)
+user2 = User(72773511)
+
+print(f'Первый пользователь: {user1}')
+print(f'Второй пользователь: {user2}')
+
+withdraw_friends = user1 & user2
+print(f'Общих друзей {len(withdraw_friends)}')
+for friend in withdraw_friends:
+    print(friend)
 
